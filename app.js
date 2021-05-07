a410bb13e7e734c74c239974276720fa91b035ce
steph0530 <steph0530@github.com>
2021-05-07 08:46:31 +0800

    next commit

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const mongoose = require('mongoose');
 const bodyParser=require('body-parser');
 const indexRouter = require('./routes/auth.routes');
 const postRouter = require('./routes/post');
+const cors=require('cors');
 //middlewares
 app.use(express.json());
+app.use(cors());
 //app.use(bodyParser.json())
 
 // view engine setup
@@ -18,12 +20,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 dotenv.config();
+
 //connect to DB
 mongoose.connect(process.env.DB_CONNECT,
 { useNewUrlParser: true },
@@ -31,7 +33,6 @@ mongoose.connect(process.env.DB_CONNECT,
   console.log('connect to DB');
   
 });
-
 //Route middlewares
 app.use('/api/user', indexRouter);
 app.use('/api/posts', postRouter);
@@ -50,8 +51,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-var port = process.env.PORT || 3000;
+//static assets 
+if(process.env.NODE_ENV==='production'){
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
+var port = process.env.PORT || 8080;
 app.listen(port,function(){
   console.log('app listening on port'+port);
 })
